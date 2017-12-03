6c51845dc8cf8e4356372411ab84afb7850f2a7e
Anthony Leif <tonyleif@hotmail.com>
2017-12-02 21:22:09 -0600

    Page 716

---
+++ WebServices/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System.Web.Mvc;
+using WebServices.Models;
+
+namespace WebServices.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ReservationRespository repo = ReservationRespository.Current;
+        public ViewResult Index()
+        {
+            return View(repo.GetAll());
+        }
+
+        public ActionResult Add(Reservation item)
+        {
+            if (ModelState.IsValid)
+            {
+                repo.Add(item);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View("View");
+            }
+        }
+
+        public ActionResult Remove(int id)
+        {
+            repo.Remove(id);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Update(Reservation item)
+        {
+            if(ModelState.IsValid && repo.Update(item))
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View("Index");
+            }
+        }
+    }
+}
---
+++ WebServices/Models/Reservation.cs
@@ -0,0 +1,10 @@
+﻿
+namespace WebServices.Models
+{
+    public class Reservation
+    {
+        public int ReservationId { get; set; }
+        public string ClientName { get; set; }
+        public string Location { get; set; }
+    }
+}
